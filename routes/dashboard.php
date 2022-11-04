613384f175a170b268ce8ce41c83a02f67f75c2d
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-11-05 01:51:56 +0600

    Dashboard

---
+++ routes/dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Online Voting System</title>
+</head>
+<body>
+    <button>Back</button>
+    <button>Log Out</button>
+    <h1>Online Voting System</h1>
+    <hr>
+    <div class="profile" id ="profile">Profile</div>
+    <div class="group" id ="group">Group</div>
+</body>
+</html>
